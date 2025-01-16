9459ed08cde82361b944bc56bec7210ff8452181
Dharizza Espinach B. <dharizza@gmail.com>
2025-01-16 19:39:58 +0000

    Use logger service in the TextTransformations class.

--- project/web/modules/custom/amd_blocks/src/TextTransformations.php
+++ project/web/modules/custom/amd_blocks/src/TextTransformations.php
@@ -10,14 +10,17 @@ namespace Drupal\amd_blocks;
 final class TextTransformations {
 
   public function reverse($text) {
+    \Drupal::logger('amd_blocks')->warning('The text was reversed.');
     return strrev($text);
   }
 
   public function uppercase($text) {
+    \Drupal::logger('amd_blocks')->warning('The text was transformed to be uppercase.');
     return strtoupper($text);
   }
 
   public function titleCase($text) {
+    \Drupal::logger('amd_blocks')->warning('The text was transformed to be title case.');
     return ucfirst($text);
   }
 
