6d950ce04d8aff7c5140f183c9344361c49c7314
Brittany Cloutier <brittcloutier@gmail.com>
2025-01-22 02:13:30 +0000

    Add image card to custom theme

--- project/web/themes/custom/tc_custom/tailwind.config.js
+++ project/web/themes/custom/tc_custom/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./{templates,components}/**/*.{js,twig}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'tilt-neon': '"Tilt Neon", sans-serif;'
+    }
   },
   plugins: [],
 }
